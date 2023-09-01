a7d0d6977e450d59ad94e9baa12853636bcec83d
Leonardo d'Angelico <74433800+dangelic@users.noreply.github.com>
2023-09-01 21:06:17 +0200

    docs: add notes for iteration formula

--- MandelbrotZoom.jl
+++ MandelbrotZoom.jl
@@ -1,3 +1,14 @@
+# z_0 = 0 + 0i
+# z_n+1 = (z_n)^2 + c (c -> starting point)
+# Will this diverge? -> if |z| >= 2 (it will grow forever)
+#       -> Color coding: 
+#       Black inside: No divergence
+#       Black outside: Direct proof
+#       Colors => Something in range
+
+
+
+
 using Plots
 f(x) = exp(-x^2/2)
 display(plot(f, -3, 3))
